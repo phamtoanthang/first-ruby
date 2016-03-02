55008f91223d11e9f00ad778ff8a8b86a663eb55
Thang Pham <phamtoanthang@gmail.com>
2016-03-02 09:53:17 +0700

    + Small change: before_filter -> before_action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-  before_filter :markdown, only: [:search, :index, :show]
+  before_action :markdown, only: [:search, :index, :show]
 
   impressionist actions: [:show], unique: [:session_hash]
 
