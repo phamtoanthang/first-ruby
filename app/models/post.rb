90d8f4bc73a255b2fd1cc673dc4f44a8b68b3212
Thang Pham <phamtoanthang@gmail.com>
2016-03-02 10:06:14 +0700

    + Fix issues with posts DEPENDING on comments when deleted. Need to delete BOTH

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,6 @@ class Post < ActiveRecord::Base
 	def self.search(search)
   		where("title LIKE ?", "%#{search}%") 
 	end
-	has_many :comments
+	# If you delete an article, its associated comments will also need to be deleted, otherwise they would simply occupy space in the database. Rails allows you to use the dependent option of an association to achieve this
+	has_many :comments, dependent: :destroy
 end
