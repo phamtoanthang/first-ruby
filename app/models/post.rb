88c18887866cd9d8aef403ed2049cd948e30500d
Thang Pham <phamtoanthang@gmail.com>
2016-03-02 10:29:39 +0700

    Add validation to prevent blank posts or comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,4 +6,5 @@ class Post < ActiveRecord::Base
 	end
 	# If you delete an article, its associated comments will also need to be deleted, otherwise they would simply occupy space in the database. Rails allows you to use the dependent option of an association to achieve this
 	has_many :comments, dependent: :destroy
+	validates :title, presence: true, length: { minimum: 5 }
 end
